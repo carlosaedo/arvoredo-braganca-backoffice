9094b4818bfa2a2543438304244c8f62688b8dca
erreboost <serviceserre@gmail.com>
2024-10-23 18:07:23 +0100

    Update SidebarRoutes.tsx

--- components/SidebarRoutes/SidebarRoutes.tsx
+++ components/SidebarRoutes/SidebarRoutes.tsx
@@ -6,7 +6,7 @@ import { dataGeneralSidebar, dataSupportSidebar } from "./SidebarRoutes.data";
 
 export function SidebarRoutes() {
   const handleButtonClick = () => {
-    window.location.href = "https://arvoredo-dashboard-braganca.vercel.app/";
+    window.location.href = "https://arvoredo-braganca.vercel.app/";
   };
 
   return (
